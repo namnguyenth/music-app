f66671aaf443ee7d47079b711a1a91be516fa223
VINGROUP\namnt70 <v.namnt70@vincom.com.vn>
2023-07-01 10:44:47 +0700

    update

--- source/modules/admin.py
+++ source/modules/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from modules.models.nation.nation import Nation
 from modules.models.album.album import Album
 from modules.models.artist.artist import Artist
+from modules.models.genre.genre import Genre
 
 admin.site.register(
     Nation
@@ -12,4 +13,7 @@ admin.site.register(
 )
 admin.site.register(
     Artist
+)
+admin.site.register(
+    Genre
 )
